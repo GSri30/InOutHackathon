e7f0cc957a3eb3d4014d9484b526cff9816697f5
jaggu21 <Siva.jagadesh@iiitb.org>
2020-12-09 15:01:53 +0530

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}>
+    </Container>
+  )
+}
+
+export default App
